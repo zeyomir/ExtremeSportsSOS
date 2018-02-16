223db70a9d3e33f351ff2a4543814f4afa1c04c4
Mirosław Zawisza <zeyomir@gmail.com>
2018-02-16 21:48:12 +0100

    clean unneeded junk in Application class

--- mobile/src/main/kotlin/io/github/zeyomir/extremesportssos/SosApplication.kt
+++ mobile/src/main/kotlin/io/github/zeyomir/extremesportssos/SosApplication.kt
@@ -2,11 +2,9 @@ package io.github.zeyomir.extremesportssos
 
 import android.app.Activity
 import android.app.Application
-import android.util.Log
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
-import io.github.zeyomir.extremesportssos.common.HelloService
 import io.github.zeyomir.extremesportssos.di.component.DaggerAppComponent
 import javax.inject.Inject
 
@@ -15,16 +13,11 @@ class SosApplication : Application(), HasActivityInjector {
     @Inject
     lateinit var dispachingAndroidInjector: DispatchingAndroidInjector<Activity>
 
-    @Inject
-    lateinit var helloService: HelloService
-
     override fun activityInjector(): AndroidInjector<Activity> = dispachingAndroidInjector
 
     override fun onCreate() {
         super.onCreate()
         initDagger()
-
-        Log.e("APPLICATION", helloService.sayHello())
     }
 
     private fun initDagger() =
