77027781b41ef4a49864bea9ad777cd18a6ae30d
Mirosław Zawisza <zeyomir@gmail.com>
2018-02-18 21:58:54 +0100

    check location settings before starting map

--- mobile/src/main/kotlin/io/github/zeyomir/extremesportssos/view/map/MapActivity.kt
+++ mobile/src/main/kotlin/io/github/zeyomir/extremesportssos/view/map/MapActivity.kt
@@ -1,19 +1,28 @@
 package io.github.zeyomir.extremesportssos.view.map
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import dagger.android.AndroidInjection
 import io.github.zeyomir.extremesportssos.R
 import io.github.zeyomir.extremesportssos.presenter.map.MapPresenter
 import io.github.zeyomir.extremesportssos.view.alarm.AlarmActivity
 import io.github.zeyomir.extremesportssos.view.main.MainActivity
 import io.github.zeyomir.extremesportssos.view.send.SendMessageActivity
+import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesProvider.REQUEST_CHECK_SETTINGS
 import kotlinx.android.synthetic.main.activity_map.*
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.OnPermissionDenied
@@ -35,12 +44,24 @@ class MapActivity : AppCompatActivity(), MapView {
 
     override fun onStart() {
         super.onStart()
-        startMonitoringLocationWithPermissionCheck()
+        checkGpsSettingsWithPermissionCheck()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                presenter.startMonitoringLocation()
+            } else {
+                Toast.makeText(this, R.string.map_gps_off, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
     override fun onDestroy() {
@@ -49,8 +70,27 @@ class MapActivity : AppCompatActivity(), MapView {
     }
 
     @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS)
-    fun startMonitoringLocation() {
-        presenter.startMonitoringLocation()
+    fun checkGpsSettings() {
+        val testLocationRequest = LocationSettingsRequest.Builder()
+                .addLocationRequest(LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY))
+                .build()
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(testLocationRequest)
+
+        task.addOnSuccessListener(this) {
+            presenter.startMonitoringLocation()
+        }
+
+        task.addOnFailureListener(this) { e ->
+            if (e is ResolvableApiException) {
+                try {
+                    e.startResolutionForResult(this@MapActivity, REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    Log.e("MapActivity", sendEx.message)
+                }
+            }
+        }
     }
 
     @OnPermissionDenied(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS)
